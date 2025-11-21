dd939364f21189a95239aebbc34430b351c8b8a7
Parkavan2003 <parkxkumar1805@gmail.com>
2025-11-21 21:03:13 +0530

    gitignore

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
 
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://tm-server-0feo.onrender.com",
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "https://tm-server-0feo.onrender.com",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
